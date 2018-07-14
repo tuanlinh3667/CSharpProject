ab75e4bf0efb99abae4b56f86f665390ee01d40a
tuanlinh3667 <linhvtd00569@fpt.edu.vn>
2018-07-14 23:54:02 +0700

    Demo ver3

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Toán tử sizeof trong C#");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("Kích cỡ kiểu int của máy tính: {0}", sizeof(int));
+            Console.WriteLine("Kích cỡ kiểu float của máy tính: {0}", sizeof(float));
+            Console.WriteLine("Kích cỡ kiểu double của máy tính: {0}", sizeof(double));
+            Console.WriteLine("Kích cỡ kiểu char của máy tính: {0}", sizeof(char));
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
