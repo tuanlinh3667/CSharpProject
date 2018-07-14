78f7afe2d0bffd9d5bd415fb18c88f4fa6db7980
tuanlinh3667 <linhvtd00569@fpt.edu.vn>
2018-07-14 23:54:20 +0700

    Demo ver4

---
+++ ConsoleApp4/ConsoleApp4/ExcuteRectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class ExcuteRectangle
+    {
+        //static void Main(string[] args)
+        //{
+        //    Rectangle r = new Rectangle();
+
+        //    r.AcceptDetailt();
+        //    r.Display();
+
+        //    Console.ReadLine();
+        //    Console.ReadKey();
+        //}
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Rectangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+
+        //contructor
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public Rectangle()
+        {
+        }
+
+
+        //get va set 
+        public double getLength()
+        {
+            return length;
+        }
+        public void setLength(double length)
+        {
+            this.length = length;
+        }
+        public double getWidth()
+        {
+            return width;
+        }
+        public void setWidth(double width)
+        {
+            this.width = width;
+        }
+
+
+        public  void AcceptDetailt()
+        {
+            Console.WriteLine("Enter Your Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your length: ", length);
+            Console.WriteLine("Your width: ", width);
+            Console.WriteLine("Your area: ", GetArea());
+        }
+    }
+}
