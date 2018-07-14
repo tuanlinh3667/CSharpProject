f5761a6eba8797524afe54a1232195a689d2e6a7
tuanlinh3667 <linhvtd00569@fpt.edu.vn>
2018-07-14 23:52:29 +0700

    Demo ver2

---
+++ ConsoleApp2/ConsoleApp2/ExecuteRectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App tinh dien tich");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("App tinh dien tich");
+
+            //khai bao doi tuong r cua lop Rectangle
+            Rectangle r = new Rectangle();
+            //goi den cac phuong thuc cuar doi tuong r
+            r.AcceptDetailt();
+            r.Display();
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ ConsoleApp2/ConsoleApp2/Ractangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Rectangle
+    {
+        //cac bien thanh vien
+        double length;
+        double width;
+
+        //phuong thuc
+        public void AcceptDetailt()
+        {
+            length = 20.5;
+            width = 50.4;
+        }
+
+        //phuong thuc
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        //phuong thuc 
+        public void Display()
+        {
+            Console.WriteLine("Chieu dai: {0}", length);
+            Console.WriteLine("Chieu rong: {0}", width);
+            Console.WriteLine("Dien tich: {0}", GetArea());
+        }
+    }
+}
